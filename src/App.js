41934bd249ba31c3da4b95c65aeb3dfdad9fedd5
Tim Jungmann <tim-j11@web.de>
2021-09-06 13:38:44 +0200

    setup basic layout and entry styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-
+import PizzaList from "./Components/PizzaList";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app-container">
+      <div className="inner-container">
+        <div className="top-left field"></div>
+        <div className="top-center field"></div>
+        <div className="top-right field"></div>
+        <div className="center-left field"></div>
+        <div className="center-center">
+          <PizzaList/>
+        </div>
+        <div className="center-right field"></div>
+        <div className="bottom-left field"></div>
+        <div className="bottom-center field"></div>
+        <div className="bottom-right field"></div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/PizzaList.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+export default function PizzaList() {
+  return (
+    <div className="pizza-list-container">
+      <div className="pizza-list-item">
+        <div className="pizza-image">
+          <img src="https://www.eat-this.org/wp-content/uploads/2020/06/eat_this_die_perfekte_vegane_pizza-22-500x500.jpg" alt="Pizza Image" />
+        </div>
+        <div className="pizza-details">
+          <p>Ingredients: Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam vero, molestias voluptas quos nostrum quis. Illum, sit dolorem odio in nam voluptatibus repellendus, deserunt provident beatae blanditiis dicta ab nesciunt?</p>
+        </div>
+        <div className="pizza-menu">
+          <div>
+            <button>1</button>
+          </div>
+          <div>
+            <button>2</button>
+          </div>
+          <div>
+            <button>3</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="pizza-list-item">
+        <div className="pizza-image">
+          <img src="https://www.eat-this.org/wp-content/uploads/2020/06/eat_this_die_perfekte_vegane_pizza-22-500x500.jpg" alt="Pizza Image" />
+        </div>
+        <div className="pizza-details">
+          <p>Ingredients: Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam vero, molestias voluptas quos nostrum quis. Illum, sit dolorem odio in nam voluptatibus repellendus, deserunt provident beatae blanditiis dicta ab nesciunt?</p>
+        </div>
+        <div className="pizza-menu">
+          <div>
+            <button>1</button>
+          </div>
+          <div>
+            <button>2</button>
+          </div>
+          <div>
+            <button>3</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="pizza-list-item">
+        <div className="pizza-image">
+          <img src="https://www.eat-this.org/wp-content/uploads/2020/06/eat_this_die_perfekte_vegane_pizza-22-500x500.jpg" alt="Pizza Image" />
+        </div>
+        <div className="pizza-details">
+          <p>Ingredients: Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam vero, molestias voluptas quos nostrum quis. Illum, sit dolorem odio in nam voluptatibus repellendus, deserunt provident beatae blanditiis dicta ab nesciunt?</p>
+        </div>
+        <div className="pizza-menu">
+          <div>
+            <button>1</button>
+          </div>
+          <div>
+            <button>2</button>
+          </div>
+          <div>
+            <button>3</button>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  )
+}
